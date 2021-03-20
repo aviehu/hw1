8539daf68f4726ef61c0bc2a90f1f421ef947bd8
Aviehu <aviehu@gmail.com>
2021-03-20 18:24:43 +0200

    p3q3

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -8,22 +8,37 @@ export const countVowels = undefined;
 /* Question 2 */
 export const runLengthEncoding : (str : string) => string = str => {
     return stringToArray(stringToArray(str).reduce((a, b) => {
-        if(a.length == 0) {
+        if(a.length === 0) {
             return a + b + 1;
         }
-        if(a[a.length - 2] != b) {
+        if(a[a.length - 2] !== b) {
             return a + b + 1;
         }
         return a.slice(0, -1) + (parseInt(a.slice(-1)) + 1);    
     },'')).reduce((a,b) => {
-        if (b == '1') {
+        if (b === '1') {
             return a;
         }
         else return a + b;
     },'');
 }
 
-console.log(runLengthEncoding('qpqqppqqqpppqqqqqpppppppp'));
 
 /* Question 3 */
-export const isPaired = undefined;
+export const isPaired: (str: string) => boolean = (str) => {
+    const open = ['(', '{', '['];
+    const close = [ ')', '}', ']']
+    return stringToArray(str).reduce((a,b) => {
+        console.log(a);
+        if (open.includes(b)) {
+            return a + b;
+        }
+        if (close.includes(b)) {
+            if ((b === ')' && a[a.length-1] !== '(') || (b === '}' && a[a.length-1] !== '{') || (b === '[' && a[a.length-1] !== ']')) {
+                return a + 'Err'
+            }
+            else return a.slice(0, -1)
+        }
+        return a;
+    },'').length == 0;
+}
