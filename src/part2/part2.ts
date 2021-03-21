02e64350bf18684d6763ac4751556d5b837123dc
Aviehu <aviehu@gmail.com>
2021-03-21 14:47:45 +0200

    better function using pipe

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,28 +1,33 @@
 import * as R from "ramda";
-
+import { pipe } from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
 export const countVowels = undefined;
 
 /* Question 2 */
-export const runLengthEncoding : (str : string) => string = str => {
-    return stringToArray(stringToArray(str).reduce((a, b) => {
-        if(a.length === 0) {
-            return a + b + 1;
-        }
-        if(a[a.length - 2] !== b) {
-            return a + b + 1;
-        }
-        return a.slice(0, -1) + (parseInt(a.slice(-1)) + 1);    
-    },'')).reduce((a,b) => {
-        if (b === '1') {
-            return a;
-        }
-        else return a + b;
-    },'');
+export const runLengthEncoding : ((str : string) => string) = str => {
+    const f = pipe(
+        ((x : string) => stringToArray(x)),
+        ((x : string[]) => x.reduce((a : String, b: string) => {
+            if(a.length === 0 || a[a.length - 2] !== b) {
+                return a + b + 1;
+            }
+            return a.slice(0, -1) + (parseInt(a.slice(-1)) + 1); 
+        },'')
+        ),
+        ((x : string) => stringToArray(x)),
+        ((x : string[]) => x.reduce((a: string, b : string) => {
+            if (b === '1') {
+                return a;
+            }
+            else return a + b; 
+        }, '' ))
+    )
+    return f(str);
 }
 
+console.log(runLengthEncoding('aaaabbbccd'));
 
 /* Question 3 */
 export const isPaired: (str: string) => boolean = (str) => {
